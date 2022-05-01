35544dca06d6c983628c8585645d623754df44b8
mlenckos <mlenckos@gmail.com>
2022-05-01 16:45:36 -0500

    did 01, 02, 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(input, value) {
+    initial_input = input;
+    if (value === 0){
+        return ""
+    }
+    if (value < 0 ){
+        return "ERROR"
+    }
+    for (i=1; i<value; i++){
+        input = initial_input+input;
+    }
+    return input
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const values = [];
+    final_string = ""
+    for(i=0; i < input.length ; i++){
+        values.push(input[i])
+    }
+    for (i=values.length-1; i >= 0 ; i--){
+        final_string = final_string + values[i]
+    }
+    return final_string
 };
 
 // Do not edit below this line
