4f02c9d5a8002cc3bde012b150b2949d9de7c11f
mlenckos <mlenckos@gmail.com>
2022-05-04 17:17:06 -0500

    did 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
 const sumAll = function() {
+    sum = 0
+    args = Array.prototype.slice.call(arguments);
+    if(arguments[0] < 0 || arguments[1] < 0){
+        return "ERROR"
+    }
+    if(typeof args[0] !== "number" || typeof args[1] !== "number" ){
+        return "ERROR"
+    }
 
+    if(arguments[0] > arguments[1]){
+        for(i = arguments[1]; i <= arguments[0]; i++){
+            sum = sum + i;
+        }
+    }
+    else{
+        for(i = arguments[0]; i <= arguments[1]; i++){
+            sum = sum + i;
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
