7f5d95a10665c887b7c021e7ec19ad9eda646096
mlenckos <mlenckos@gmail.com>
2022-05-05 16:36:34 -0500

    finished temp check

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
 const leapYears = function() {
-
+    // if divisible by 4 then leap year
+    // if divisible by 100, then not leap year unless divisible by 400
+    args = Array.prototype.slice.call(arguments);
+    if(args[0] % 4 === 0){
+        if(args[0] % 400 === 0){
+            console.log("hit 1")
+            return true
+        }
+        if(args[0] % 100 === 0){
+            console.log("hit 2")
+            return false
+        }
+        else{
+            console.log("hit 3")
+            return true
+        }
+    }
+    else{
+        console.log("hit 4")
+        return false
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
 const ftoc = function() {
-
+  // ([°F] − 32) × 5⁄9
+  args = Array.prototype.slice.call(arguments)
+  answer = (args[0]-32)*(5/9)
+  console.log(typeof(answer))
+  rounded = Math.round( answer * 10 ) / 10
+  return rounded
 };
 
 const ctof = function() {
-
+  // [°C] × 9⁄5 + 32
+  args = Array.prototype.slice.call(arguments)
+  answer = (args[0]*(9/5)+32)
+  rounded = Math.round( answer * 10 ) / 10
+  return rounded
 };
 
 // Do not edit below this line
